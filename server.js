211842d68025d1397d3024a5636ed1cef493b88a
Karuna Baral <karuna.baral@foxsports.com.au>
2016-09-10 11:35:50 +1000

    Step 7: Sending the blogpost data toserver

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // serve all the assets in our public folder
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.post('/create-post', function (req, res){
+	console.log('/create-post');
+	console.log(req.body);
+
+	res.redirect('/');
+});
 
 app.listen(3000, function() {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
