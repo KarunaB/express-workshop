e8987d34e4b844ce3864646a7762366d68abc943
Karuna Baral <karuna.baral@foxsports.com.au>
2016-09-10 11:21:01 +1000

    Step 6: Serving static files, removed app.get statements

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hello world!');
-});
-
-app.get('/about', function(req, res) {
-	res.send('About this blog!');
-});
+// serve all the assets in our public folder
+app.use(express.static('public'));
 
 app.listen(3000, function() {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
 });
-
