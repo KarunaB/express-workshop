207add6f7a75feac1eaa8c7520e6e607c708086b
Karuna Baral <karuna.baral@foxsports.com.au>
2016-09-10 12:07:42 +1000

    Step 8: Saved blogpost to json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 // serve all the assets in our public folder
 app.use(express.static('public'));
@@ -10,8 +11,29 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/create-post', function (req, res){
-	console.log('/create-post');
-	console.log(req.body);
+	fs.readFile(__dirname + '/data/posts.json',
+		function (error, file) {
+			if (error || !req.body.blogpost) {
+				console.log('Error reading file!');
+				return;
+			}
+			// console.log(file.toString());
+
+			var parsedFile = JSON.parse(file);
+
+			// Update the data with newly added post
+			parsedFile[Date.now()] = req.body.blogpost;
+
+			// Now convert JSON to string
+			// and write it to the file
+			fs.writeFile(__dirname + '/data/posts.json',
+				JSON.stringify(parsedFile),
+				function (error) {
+					console.log('error', error);
+				}
+			);
+		}
+	);
 
 	res.redirect('/');
 });
