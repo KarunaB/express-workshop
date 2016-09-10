137488d805cbe50e0d49ff5ef4a22aa4aca41368
Karuna Baral <karuna.baral@foxsports.com.au>
2016-09-10 11:15:12 +1000

    Step 5: Added my own endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
 	res.send('Hello world!');
 });
 
+app.get('/about', function(req, res) {
+	res.send('About this blog!');
+});
+
 app.listen(3000, function() {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
 });
